b56ade335a5e12bf4152bc3d08e54e2c313eae2e
Ellie Day <heyellieday@gmail.com>
2014-11-16 21:39:32 -0500

    Refactored into class

--- shortest_route.rb
+++ shortest_route.rb
@@ -2,65 +2,83 @@
 #It takes 1 mins to travel from one point to another point.
 
 
-def shortest_route(coordinates_array, time, start_coordinates)
-	grid = Array.new(100){ |i| 
-		i = Array.new(100){ |i|
-		 i = "x"
-		} 
-	}
-	shortest_routes = []
+class ShortestRoute
+	
+	def initialize(coordinates_array, time, start_coordinates)
+		@coordinates_array = coordinates_array
+		@time = time
+		@start_coordinates = start_coordinates
+		@grid = Array.new(100){ |i| 
+			i = Array.new(100){ |i|
+			 i = "x"
+			} 
+		}
+		@shortest_route = []
+		if (time % 15 != 0) 
+			puts "Please enter a valid time."
+			return
+		end
+		find_distances
 
-	if (time % 15 != 0) 
-		puts "Please enter a valid time."
-		return
 	end
-	coordinates_array.each_with_index do |coordinates, index|
 
-		coordinates_array.each_with_index do |coordinates_compare, index_compare|
+	def coordinates_array
+		return @coordinates_array
+	end
 
-			#distance between
-			if (index != index_compare)
-				a = coordinates[:x] - coordinates_compare[:x]
-				b = coordinates[:y] - coordinates_compare[:y]
-				distance = Math.sqrt((a*a) + (b*b)).floor
-				coordinates_array[index][:distances].push({
-					x: coordinates_compare[:x],
-					y: coordinates_compare[:y],
-					distance: distance,
-					visited: false})
-					puts distance
+	def find_distances
+		@coordinates_array.each_with_index do |coordinates, index|
+
+			@coordinates_array.each_with_index do |coordinates_compare, index_compare|
+
+				#distance between
+				if (index != index_compare)
+					a = coordinates[:x] - coordinates_compare[:x]
+					b = coordinates[:y] - coordinates_compare[:y]
+					distance = Math.sqrt((a*a) + (b*b)).floor
+					@coordinates_array[index][:distances].push({
+						x: coordinates_compare[:x],
+						y: coordinates_compare[:y],
+						distance: distance,
+						visited: false})
+						puts distance
+
+				end
 
 			end
 
+			@grid[coordinates[:x]][coordinates[:y]] = "coordinates:  #{coordinates[:x]}, #{coordinates[:y]}"
 		end
-
-		grid[coordinates[:x]][coordinates[:y]] = "coordinates:  #{coordinates[:x]}, #{coordinates[:y]}"
 	end
-	coordinates_array.each_with_index do |coordinates, index|
-		if (coordinates_array[index][:visited] == false )
-			coordinates_array.each_with_index do |coordinates_compare, index_compare|
-				if (coordinates_array[index_compare][:visited] == false )
-				#shortest routes
-					if (index != index_compare)
-						shortest_distance = nil
-						coordinates_array[index][:distances].each_with_index do |distance, index_distance|
-							if shortest_distance == nil
-								shortest_distance = 1000
-							end
-							if (shortest_distance >= distance[:distance] && distance[:visited] == false )
-								shortest_distance = distance[:distance]
-								coordinates_array[index][:distances][index_distance][:visited] = true
-								coordinates_array[index_distance][:visited] = true
-								shortest_routes.push(shortest_distance)
-							end
-						end
+	
+
+	def calculate_shortest_route(coordinates_array, state)
+		if (@coordinates_array[state][:visited] == false )
+			#shortest routes
+			if (index != index_compare)
+				shortest_route = nil
+				@coordinates_array[index][:distances].each_with_index do |distance, index_distance|
+					if shortest_distance == nil
+						shortest_distance = 1000
+					end
+					if (shortest_distance >= distance[:distance] && distance[:visited] == false )
+						shortest_distance = distance[:distance]
+						@coordinates_array[index][:distances][index_distance][:visited] = true
+						@coordinates_array[index_distance][:visited] = true
+						shortest_routes.push(shortest_distance)
 					end
 				end
 			end
 		end
+		state.next
+		if state == 5 
+		 return
+		end
+		calculate_shortest_distance(@coordinates_array)
+		print shortest_distance
 	end
-	print shortest_routes
 end
+
 
 coordinates_array = [
 	{x: 86, y: 74, distances: [], visited: false},
@@ -76,7 +94,9 @@ coordinates_array = [
 ]
 
 #time in 15 minute blocks
-time = 120 
+time = 120
 start_coordinates = {x: 1, y: 3}
 
-shortest_route(coordinates_array, time, start_coordinates)
+shortest_route = ShortestRoute.new(coordinates_array, time, start_coordinates)
+shortest_route.calculate_shortest_route(shortest_route.coordinates_array, 0)
+
